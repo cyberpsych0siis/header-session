c832b025f50f6a3446d1e4ddc9a2a452e67a6768
Philipp Gaßner <thallosaurus>
2022-03-16 20:38:11 +0100

    first commit

---
+++ session.js
@@ -0,0 +1,37 @@
+"use strict";
+
+import crypto from 'crypto';
+
+import RedisStore from './session/redis.js';
+
+export default function session(redisClient) {
+  return function hmm(req, res, next) {
+    if (req.url === "/health") {
+      next();
+      return;
+    }
+
+    const sid = req.headers["x-session"];
+
+    req.sessionID = sid;
+    req.sessionStore = new RedisStore(redisClient.store.redisClient, sid);
+    // req.session =
+      
+    req.sessionStore.getSession((data) => {
+      console.log(data);
+      req.session = data;
+      next();
+      // return;
+      });
+
+    const end = res.end;
+    
+    res.end = function (chunk, encoding) {
+      
+      // const currentHash = crypto.createHash('md5').update(JSON.stringify(req.session)).digest('hex');
+        
+      req.sessionStore.update(req.session);
+      end.call(res, chunk, encoding);
+    }
+  }
+}
---
+++ store.js
@@ -0,0 +1,62 @@
+// const Store = require("./store")
+
+export default class RedisStore {
+  options = {};
+
+  sessionPrefix = "sess_";
+
+  constructor(redisClient, sessionId) {
+    // for (let i = 0; i < Object.keys(options); i++) {}
+    // this.redis = redis;
+    // super();
+
+    this.sessionId = sessionId;
+    // this.storage = new Map();
+    this.redisClient = redisClient;
+  }
+
+  create(callback) {
+    // module.exports.createNewSessionToken = function (timeoutInSeconds) {
+    if (this.sessionId === null) {
+      const token = crypto.randomBytes(16).toString("hex");
+      this.redisClient.set(this.sessionPrefix + token, "{}");
+      // };
+      // this.redisClient;
+      this.sessionId = token;
+      callback(token);
+    } else throw new Error("Already owns a session");
+  }
+
+  destroy(callback) {
+    console.log("Destroy " + this.sessionPrefix + this.sessionId);
+    this.redisClient.del(this.sessionPrefix + this.sessionId);
+    this.sessionId = null;
+    callback(null);
+  }
+
+  update(session) {
+    // console.log(JSON.stringify(this));
+    this.redisClient.set(
+      this.sessionPrefix + this.sessionId,
+      JSON.stringify(session)
+    );
+  }
+
+  getSession(callback) {
+    // return
+    this.redisClient.get(this.sessionPrefix + this.sessionId).then((result) => {
+      //   console.log(arguments);
+      callback(JSON.parse(result));
+    });
+  }
+
+  async sessionExists() {
+    return await this.redisClient.exists(this.sessionPrefix + this.sessionId);
+  }
+
+  //TODO Implement TTL Touch
+  /*   touch(sid, session, callback) {
+    // console.log(sid, session);
+    callback(null);
+  } */
+}
